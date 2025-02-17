9a7469d65ac77838cd8d81e5276d0ed9a88f0411
Mariem112001 <abouda.mariem@esprit.tn>
2025-02-17 12:06:31 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/tpfoyer/entity/Foyer.java
@@ -0,0 +1,23 @@
+package tn.esprit.tpfoyer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Foyer implements Serializable {
+    @Id
+    private Long idFoyer;
+    private String nomFoyer;
+    private Long capaciteFoyer;
+
+}
